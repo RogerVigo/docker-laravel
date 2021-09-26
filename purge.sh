4ac02bc5cfb2e6e83ad0ae4b9f048f299b952684
Roger <rogelio.alonso@icabbi.com>
2021-09-26 12:43:13 +0200

    Added redis and installation of mysql client

--- purge.sh
+++ purge.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-docker rmi project:laravel project:nginx
 docker volume rm laravel
 docker network rm laravel
 
-docker stop laravel-php laravel-nginx laravel-mysql
+docker stop laravel-php laravel-nginx laravel-mysql laravel-redis
 
 docker rm laravel-php
 docker rm laravel-nginx
 docker rm laravel-mysql
+docker rm laravel-redis
+
+docker rmi project:laravel project:nginx
